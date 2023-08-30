6fafb81374e63808ab0a9d7df4147749fc15ec12
Jack Clayton <jack@modular.com>
2023-08-29 21:48:37 -0700

    [mojo-examples] Fix interop in exmamples (#20109)
    Fixes issue [Internal Link] and
    [Internal Link]
    
    Will look for the modules in the current directory or ./examples, in
    case the user is running from the root of the repo, and print an error
    if it can't find the required modules.
    
    It will also install numpy if it's missing, as failing when missing a
    pip module currently doesn't give an informative error.
    
    modular-orig-commit: 7ae024748887fe3e7dbd54b84bd58d5509f91739

--- examples/code/pymatmul.py
+++ examples/code/pymatmul.py
@@ -13,6 +13,14 @@
 
 # Simple program demonstrating a naive matrix multiplication in Python
 
+import importlib
+import sys
+import subprocess
+
+if not importlib.find_loader("numpy"):
+    print("Numpy not found, installing...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy"])
+
 import numpy as np
 from timeit import timeit
 
