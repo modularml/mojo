13d29eb037597d69dd16f6d7d372d054187f0582
Joe Loser <joe@modular.com>
2024-03-05 23:19:46 -0700

    [mojo-stdlib] Allow overriding just-built stdlib in `lit.cfg.py` (#33912)
    External contributors need a way to specify the just-built stdlib when
    running the stdlib unit tests. Add this functionality by leveraging the
    fact that our configuration-parsing code allows for overrides via an
    environment variable. Set `MODULAR_MOJO_NIGHTLY_IMPORT_PATH` environment
    variable to specify the `-I` paths we want when running the tests:
    - The path to the just-built-stdlib
    - The path the `test_utils` which is needed by some tests
    
    While here, add the necessary propagation of several other environment
    variables we need to properly run the unit tests, such as `MODULAR_HOME`
    and friends.
    
    Fixes [Internal Link]
    
    modular-orig-commit: a36770ab8a4dbfd5fc5d3f107af3daeecfb4bab5

--- stdlib/test/open-source-lit.cfg.py
+++ stdlib/test/open-source-lit.cfg.py
@@ -5,8 +5,10 @@
 # ===----------------------------------------------------------------------=== #
 
 from pathlib import Path
+import os
 
 import lit.formats
+import lit.llvm
 
 config.test_format = lit.formats.ShTest(True)
 
@@ -27,3 +29,26 @@ config.test_exec_root = Path.cwd()
 # since we're not supporting `--sanitize` initially
 # to allow running the tests with LLVM sanitizers.
 config.substitutions.insert(0, ("%mojo", "mojo"))
+
+test_root = Path.cwd().parent
+stdlib_root = Path.cwd().parent.parent
+os.environ["MODULAR_MOJO_NIGHTLY_IMPORT_PATH"] = (
+    str(stdlib_root) + "," + str(test_root)
+)
+
+# Pass through several environment variables
+# to the underlying subprocesses that run the tests.
+lit.llvm.initialize(lit_config, config)
+lit.llvm.llvm_config.with_system_environment(
+    [
+        "HOME",
+        "INCLUDE",
+        "LIB",
+        "PATH",
+        "MODULAR_HOME",
+        "MODULAR_MOJO_NIGHTLY_IMPORT_PATH",
+        "TEMP_DIR",
+        "TMP",
+        "TEMP",
+    ]
+)
