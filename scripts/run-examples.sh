1df33b408ada27d8b51795d29154c3dcd690f0ae
Joe Loser <joeloser@fastmail.com>
2024-03-15 08:34:02 -0600

    [Examples] Add ability to run and test examples
    Add a `lit.cfg.py` in the `examples` directory which sets up the lit config for
    example Mojo files (notebooks not yet supported).  Add a wrapper script,
    `run-examples.sh` which creates a build directory and invokes `lit` using this
    configuration.
    
    Go one step further and create a GitHub workflow that puts all of the pieces
    together to download the latest stable Mojo compiler and test the examples using
    the provided script.
    
    The `scripts/run-examples.sh` is something that can be run both locally and in
    CI.
    
    Add common artifacts to the `.gitignore` such as the new `build` directory, and
    other Python-related things.

---
+++ scripts/run-examples.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT="${SCRIPT_DIR}"/../
+EXAMPLES_DIR="${REPO_ROOT}"/examples
+
+BUILD_DIR="${REPO_ROOT}"/build
+mkdir -p "${BUILD_DIR}"
+
+# Run the examples using `lit`.
+lit -sv "${EXAMPLES_DIR}"
