f613b28c7ff83a79797a431162d66781f38c3ef5
Joe Loser <joe@modular.com>
2024-03-20 15:08:32 -0600

    [mojo-stdlib] Rename `stdlib-open-source` to `stdlib` (#35262)
    Rename `scripts/stdlib-open-source` to just `scripts/stdlib` and update the
    consumers everywhere.
    
    modular-orig-commit: 9f2d081ff23627d5d0350e47c2cd628c704ae7c8

---
+++ stdlib/scripts/build-stdlib.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##===----------------------------------------------------------------------===##
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT="${SCRIPT_DIR}"/../..
+BUILD_DIR="${REPO_ROOT}"/build
+mkdir -p "${BUILD_DIR}"
+
+STDLIB_PATH="${REPO_ROOT}/stdlib/src"
+
+echo "Packaging up the Standard Library."
+STDLIB_PACKAGE_NAME="stdlib.mojopkg"
+FULL_STDLIB_PACKAGE_PATH="${BUILD_DIR}"/"${STDLIB_PACKAGE_NAME}"
+mojo package "${STDLIB_PATH}" -o "${FULL_STDLIB_PACKAGE_PATH}"
+
+echo Successfully created "${FULL_STDLIB_PACKAGE_PATH}"
---
+++ stdlib/scripts/check-doc-strings.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+##===----------------------------------------------------------------------===##
+#
+# This file is Modular Inc proprietary.
+#
+##===----------------------------------------------------------------------===##
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT="${SCRIPT_DIR}/.."
+
+check_doc_string() {
+  local pkg=$1
+  echo "Checking API doc string conformance for package ${pkg}"
+
+  local warnings_file="${BUILD_DIR}/${pkg}_warnings.txt"
+  rm -f "${warnings_file}"
+  mojo doc -warn-missing-doc-strings -o /dev/null "${REPO_ROOT}/${pkg}" > "${warnings_file}" 2>&1
+  python3 "${SCRIPT_DIR}"/check-file-is-empty.py "${warnings_file}"
+}
+
+BUILD_DIR="${REPO_ROOT}"/build
+mkdir -p "${BUILD_DIR}"
+
+check_doc_string stdlib
+check_doc_string test_utils
+
