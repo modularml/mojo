ea9f6ceeb7172f64704c904bbc62c0b198184c86
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2024-04-15 19:14:05 +0000

    Add the bash file
    Signed-off-by: gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>

---
+++ stdlib/scripts/spellcheck-notebooks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for arg in "$@"
+do
+  output=$(jupyter nbconvert --log-level ERROR --to markdown "$arg" --stdout)
+  if [ $? -ne 0 ]; then
+    echo "--> Failed to convert $arg to markdown"
+    exit 1
+  fi
+  echo "$output" | codespell -L inout -
+  if [ $? -ne 0 ]; then
+    echo "--> Spelling errors found in $arg"
+    exit 1
+  fi
+done
