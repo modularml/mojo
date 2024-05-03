6583f9f287b7557006dd9b1b3e2b074ca0e50bcd
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2024-05-03 09:27:43 -0600

    [External] [tools] Add a docstring check to the pre-commit (v2) (#39249)
    [External] [tools] Add a docstring check to the pre-commit (v2)
    
    Follow up on #2343
    
    This will run automatically in the CI and for contributors using
    pre-commit :)
    
    Fixes #2331
    
    Co-authored-by: gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
    Closes modularml/mojo#2483
    MODULAR_ORIG_COMMIT_REV_ID: dd111ea538e50db3f2e0748e90501c779606dc75

---
+++ stdlib/scripts/check-docstrings.py
@@ -0,0 +1,40 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+
+import subprocess
+import sys
+
+# TODO: Use the "mojo doc" directly when there is an option to
+# fail if warnings are present (something like -Werror for gcc).
+
+
+def main():
+    # This is actually faster than running "mojo doc" on each file since
+    # "mojo doc" only accept a single file/path as argument
+    command = [
+        "mojo",
+        "doc",
+        "--warn-missing-doc-strings",
+        "-o",
+        "/dev/null",
+        "./stdlib/src",
+    ]
+    result = subprocess.run(command, capture_output=True)
+    if result.stderr or result.returncode != 0:
+        print(f"Docstring issue found in the stdlib: ")
+        print(result.stderr.decode())
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
