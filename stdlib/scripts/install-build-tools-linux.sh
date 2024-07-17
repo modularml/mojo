2a3e57ab407a2d06ef06e77198d129c75117b2d9
modularbot <116839051+modularbot@users.noreply.github.com>
2024-07-16 19:01:31 -0500

    [External] [GitHub Actions] Extract install build tools into separate scripts (#43435)
    [External] [GitHub Actions] Extract install build tools into separate
    scripts
    
    Related to https://github.com/modularml/mojo/issues/3200.
    
    Putting the install scripts as separate files would allow the users to
    easily install the dependencies.
    
    The setup instructions will be added to the contributing guide later.
    
    ORIGINAL_AUTHOR=Haifeng Jin
    <5476582+haifeng-jin@users.noreply.github.com>
    PUBLIC_PR_LINK=modularml/mojo#3234
    
    Co-authored-by: Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
    Closes modularml/mojo#3234
    MODULAR_ORIG_COMMIT_REV_ID: ad31b57a05d286a25f3679b638f7a46068a90b05

---
+++ stdlib/scripts/install-build-tools-linux.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+##===----------------------------------------------------------------------===##
+# Copyright (c) 2024, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##===----------------------------------------------------------------------===##
+set -euo pipefail
+
+LLVM_VERSION=17
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh $LLVM_VERSION
+rm llvm.sh
+
+# Make common LLVM binaries (including FileCheck) in our PATH so they work when used in an unversioned context
+# For example, instead of saying `FileCheck-17` which exists in `/usr/bin`, this allows us to just call
+# FileCheck unqualified.
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$LLVM_VERSION 100
+sudo update-alternatives --install /usr/bin/lld lld /usr/bin/lld-$LLVM_VERSION 100
+sudo update-alternatives --install /usr/bin/ld.lld ld.lld /usr/bin/ld.lld-$LLVM_VERSION 100
+sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-$LLVM_VERSION 100
+sudo update-alternatives --install /usr/bin/FileCheck FileCheck /usr/bin/FileCheck-$LLVM_VERSION 100
+
+python3 -m pip install lit
