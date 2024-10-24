5aa1e51d0dcdc6f68379cb31133eda809dedc375
martinvuyk <martin.vuyklop@gmail.com>
2024-10-24 17:15:41 -0300

    mojo format
    Signed-off-by: martinvuyk <martin.vuyklop@gmail.com>

--- stdlib/benchmarks/lit.cfg.py
+++ stdlib/benchmarks/lit.cfg.py
@@ -61,12 +61,12 @@ else:
     # with the Mojo SDK to the appropriate environment variables.
     # These environment variables are interpreted by the mojo parser
     # when resolving imports.
-    os.environ["MODULAR_MOJO_NIGHTLY_IMPORT_PATH"] = (
-        f"{build_root},{pre_built_packages_path}"
-    )
-    os.environ["MODULAR_MOJO_MAX_NIGHTLY_IMPORT_PATH"] = (
-        f"{build_root},{pre_built_packages_path}"
-    )
+    os.environ[
+        "MODULAR_MOJO_NIGHTLY_IMPORT_PATH"
+    ] = f"{build_root},{pre_built_packages_path}"
+    os.environ[
+        "MODULAR_MOJO_MAX_NIGHTLY_IMPORT_PATH"
+    ] = f"{build_root},{pre_built_packages_path}"
 
     # Pass through several environment variables
     # to the underlying subprocesses that run the tests.
