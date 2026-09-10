abbc4bd06fd7d817a0d3c31e1ad9ef321ae30d35
Brad Larson <bradlarson@modular.com>
2026-09-10 18:00:39 -0500

    [lint] Add missing licenses for C files Several C files were missing Apache 2.0 licenses after the open-sourcing of the compiler.
    MODULAR_ORIG_COMMIT_REV_ID: 79bf2cc27efaea3602471d6e2bd0e47e97ac6e34

--- Mojo/test/mojo-integration/extern-c-abi/c_abi_test_int_structs.c
+++ Mojo/test/mojo-integration/extern-c-abi/c_abi_test_int_structs.c
@@ -1,3 +1,15 @@
+//===----------------------------------------------------------------------===//
+// Copyright (c) 2026, Modular Inc. All rights reserved.
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions:
+// https://llvm.org/LICENSE.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===----------------------------------------------------------------------===//
 // C ABI integer struct tests
 // Mojo tests: test_struct_argument_{1,2,4,8,9,12,16,17,int_31,33}byte.mojo
 // Tests integer-only struct classification and register passing
